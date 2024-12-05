306a5930da79ed7781a509cb91f018d223ceba87
Genesis <genesisgalileah@arizona.edu>
2024-12-05 10:50:08 -0700

    Worked on reservations page

--- scripts/reservations.js
+++ scripts/reservations.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html> 
-<html lang = "en">
-<head>
-  <title>Reservations</title>;
-  <meta charset = "utf-8">;
-  <link rel="stylesheet" href="style.css">;
-</head>
-  
-<body>
 
-  
-</body>
-  
-</html>
